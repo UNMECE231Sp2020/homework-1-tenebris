beaa5bf3a89488793361aa618f863d4f06e3b68e
Veloraxzia <impendingdoom.taco@gmail.com>
2020-02-06 09:50:25 -0700

    real bruh moment

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main() {
 	Complex (*hand1[4])(Complex c1, Complex c2) = {complex_add, complex_sub, complex_mult, complex_div};
 	double (*hand2[2])(Complex c) = {magnitude, phase};
 
-	for(int i = 0; i < 5; i++) {
+	for(int i = 0; i < 4; i++) {
 		funct1(hand1[i], a, b);
 	}
 
