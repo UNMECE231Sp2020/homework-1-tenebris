7a64ff0e1be9e926d576d26f7fd524431463bb7c
Veloraxzia <impendingdoom.taco@gmail.com>
2020-02-06 09:53:06 -0700

    Cleaned up code

--- complex_fns.c
+++ complex_fns.c
@@ -52,9 +52,8 @@ Complex complex_div(Complex c1, Complex c2) {
 	return c;
 }
 
-void funct1(Complex (*funct)(Complex, Complex), Complex c1, Complex c2) {
+void print4(Complex (*funct)(Complex, Complex), Complex c1, Complex c2)
 	print_complex(funct(c1, c2));
-}
-void funct2(double (*funct)(Complex), Complex c) {
+
+void print2(double (*funct)(Complex), Complex c)
 	printf("%f\n", funct(c));
-}
--- main.c
+++ main.c
@@ -8,13 +8,11 @@ int main() {
 	Complex (*hand1[4])(Complex c1, Complex c2) = {complex_add, complex_sub, complex_mult, complex_div};
 	double (*hand2[2])(Complex c) = {magnitude, phase};
 
-	for(int i = 0; i < 4; i++) {
-		funct1(hand1[i], a, b);
-	}
+	for(int i = 0; i < 4; i++)
+		print4(hand1[i], a, b);
 
-	for(int i = 0; i < 2; i++) {
-		funct2(hand2[i], a);
-	}
+	for(int i = 0; i < 2; i++)
+		print2(hand2[i], a);
 
 	return 0;
 }
